efd7cd44c903a3e32244b65072f806b31787ffaa
Tim Lin <e0610k@gmail.com>
2021-11-06 12:48:48 +0800

    delete some useless code

--- all.js
+++ all.js
@@ -1,28 +1,4 @@
 // ----- 變數 -----
-const cityData = [
-  { name: '臺北市', value: 'Taipei' },
-  { name: '新北市', value: 'NewTaipei' },
-  { name: '桃園市', value: 'Taoyuan' },
-  { name: '臺中市', value: 'Taichung' },
-  { name: '臺南市', value: 'Tainan' },
-  { name: '高雄市', value: 'Kaohsiung' },
-  { name: '基隆市', value: 'Keelung' },
-  { name: '新竹市', value: 'Hsinchu' },
-  { name: '新竹縣', value: 'HsinchuCounty' },
-  { name: '苗栗縣', value: 'MiaoliCounty' },
-  { name: '彰化縣', value: 'ChanghuaCounty' },
-  { name: '南投縣', value: 'NantouCounty' },
-  { name: '雲林縣', value: 'YunlinCounty' },
-  { name: '嘉義縣', value: 'ChiayiCounty' },
-  { name: '嘉義市', value: 'Chiayi' },
-  { name: '屏東縣', value: 'PingtungCounty' },
-  { name: '宜蘭縣', value: 'YilanCounty' },
-  { name: '花蓮縣', value: 'HualienCounty' },
-  { name: '臺東縣', value: 'TaitungCounty' },
-  { name: '金門縣', value: 'KinmenCounty' },
-  { name: '澎湖縣', value: 'PenghuCounty' },
-  { name: '連江縣', value: 'LienchiangCounty' },
-]
 let filterObj = {}
 let busData = []
 let forthData = []
@@ -49,16 +25,6 @@ const apiBusRequest = axios.create({
 const apiEstimateTimeGet = () => apiBusRequest.get(`/EstimatedTimeOfArrival/City/${filterObj['filterCity']}/${filterObj['filterRouteName']}?$format=JSON`)
 
 const apiStopRouteGet = () => apiBusRequest.get(`/StopOfRoute/City/${filterObj['filterCity']}/${filterObj['filterRouteName']}?$format=JSON`)
-
-// ----- 選擇地區加入 option -----
-function pushCityOption() {
-  let option = '<option value="null">請選擇</option>'
-  cityData.forEach(item => {
-    option += `<option value="${item['value']}">${item['name']}</option>`
-  })
-  citySelect.innerHTML = option
-}
-pushCityOption()
 
 // ----- 選擇地區、公車路線號碼 -----
 function filterCitySelect(e) {
